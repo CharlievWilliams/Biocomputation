b824343188f93a6c0d6a2d2cb8055a615698e01a
Charlie Williams <charliewilliamsweb@gmail.com>
2020-12-08 15:06:55 +0000

    Create MinimisationFunction.py and MinimisationFunctionTwo.py

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from CountingReals import CountReals
+from MinimisationFunction import PerformMinimisationFunction
+from MinimisationFunctionTwo import PerformMinimisationFunctionTwo
 
 
 def main():
-    CountReals()
+    PerformMinimisationFunctionTwo()
 
 
 main()
